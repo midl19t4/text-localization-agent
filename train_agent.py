d1fdb4f07cc794538a2223dc1fe64d543954e214
MariusDanner <marius@danner.online>
2019-05-22 11:35:52 +0200

    Make forward method robust to batch size > 1

--- train_agent.py
+++ train_agent.py
@@ -28,13 +28,12 @@ class CustomModel(Chain):
 
 
     def forward(self, x):
-        #import pdb; pdb.set_trace()
         image, history, penalty = x[0], x[1], x[2]
-        image = F.reshape(image, (1,3,224,224))
-        history = F.reshape(history.astype('float32'),(1,-1))
-        penalty = F.reshape(penalty,(1,-1))
+        image = F.reshape(image, (-1,3,224,224))
+        history = F.reshape(history.astype('float32'),(-1,90))
+        penalty = F.reshape(penalty,(-1,1))
         h1 = F.relu(self.resNet(image, layers=['pool5'])['pool5'])
-        h1 = F.reshape(F.concat((h1, history, penalty), axis=1), (1,-1))
+        h1 = F.reshape(F.concat((h1, history, penalty), axis=1), (-1,2139))
         h2 = F.relu(self.l1(h1))
         h3 = F.relu(self.l2(h2))
         return F.relu(self.l3(h3))
