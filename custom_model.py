1baae906be17871428c1f620daf14256521d2eb9
jak-ing <lupin2702@gmail.com>
2019-05-29 12:16:22 +0200

    Use group norm ResNet instead of batch norm ResNet to avoid pointless batch normalization for batch size = 1

--- custom_model.py
+++ custom_model.py
@@ -1,12 +1,13 @@
 from chainer import functions as F
 from chainer import links as L
 from chainer import Chain
+from resnet_group_norm import ResNet as ResNetGroupNorm
 
 class CustomModel(Chain):
     def __init__(self, n_actions):
         super(CustomModel, self).__init__()
         with self.init_scope():
-            self.resNet=L.ResNet50Layers()
+            self.resNet=ResNetGroupNorm(n_layers=18)
             self.l1=L.Linear(2138, 1024)
             self.l2=L.Linear(1024, 1024)
             self.l3=L.Linear(1024, n_actions)
